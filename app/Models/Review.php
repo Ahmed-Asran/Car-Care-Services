74c227106082fb1e306720a70104d45184ab670d
Modyshebl <148388547+Modyshebl@users.noreply.github.com>
2025-08-21 20:19:59 +0300

    Update Review.php

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = ['request_id', 'rating', 'comment'];
+
+    public function request()
+    {
+        return $this->belongsTo(Request::class);
+    }
 }
