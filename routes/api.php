53d4ca4c97cd5c761d12893cee3159c39a221813
Abdallahalaa877 <abdallahalaa877@gmail.com>
2025-08-21 22:32:29 +0300

    updated Enquiry

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageController;
+use App\Http\Controllers\EnquiryController;
+use App\Http\Controllers\EnquiryResponseController;
 
 
 /// IMAGE
 Route::post('/images', [ImageController::class, 'store']);
 
+Route::post('enquiries', [EnquiryController::class, 'store']);     // POST إضافة استفسار
+
+Route::post('enquiry-responses', [EnquiryResponseController::class, 'store']);
 
 ///SETTING
 
