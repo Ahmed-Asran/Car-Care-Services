74598001671fcd887a15c48629a84bae71718276
Modyshebl <148388547+Modyshebl@users.noreply.github.com>
2025-08-23 18:12:02 +0300

    Added and completed relationships

--- app/Models/CarType.php
+++ app/Models/CarType.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class CarType extends Model
 {
  protected $fillable = ['manufacturer', 'model'];
+
+     use HasFactory;
+
+    public function cars()
+    {
+        return $this->hasMany(CustomerCar::class);
+    }
+
+    public function serviceMappings()
+    {
+        return $this->hasMany(ServiceForCarType::class);
+    }
+    
 }
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -2,11 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Location extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'street', 'city', 'state', 'country', 'latitude', 'longitude'
     ];
+
+    public function providers()
+    {
+        return $this->hasMany(Provider::class);
+    }
 }
